c3a8a6fb0647f2f5325b0ba8a2f0c9010a85162e
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-12 19:59:56 +0200

    Test rutas teams

--- routers/auth.js
+++ routers/auth.js
@@ -31,7 +31,8 @@ router.route('/login')
             // Si son válidas, generamos un JWT y lo devolvemos
             const secretKey = 'secretPassword';
             let user = usersController.getUserIdFromUserName(req.body.user);
-            const token = jwt.sign({ userId: user.userId }, secretKey)
+            const token = jwt.sign({ userId: user.userId }, secretKey);
+            // Envía una respuesta HTTP con un estado 200 (éxito) y un cuerpo JSON. El objeto JSON que se pasa como argumento a json() se serializa y se envía en el cuerpo de la respuesta.
             res.status(200).json(
                 {
                     // Token que contiene la clave secreta: secretPassword, un header con: ALGORITHM & TOKEN TYPE y un PAYLOAD:DATA con: el nombre del usuario
